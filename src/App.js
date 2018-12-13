56f1bdc45f0f7c0e1bdd1703a7a5392d9fe1a254
Zumwaloro <bulattila@hotmail.com>
2018-12-13 16:19:15 +0100

    Initial stage of project with a nearly finished index page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import headerIndex from './img/headers/header-index.png';
+import homeWelcome from './img/stock/home.jpg';
+
+function Header({headRef, headId}) {
+  return(
+    <div>
+      <header id="home">
+         <img src={headRef} id={headId}></img>
+      </header>
+      <LinkIcon icon="fab fa-facebook-square" link="https://www.facebook.com/Vingulmarkhaerkampgruppe/" idRef="face-icon" />
+    </div>
+  );
+}
+
+function Button({label}) {
+  return(
+    <button id="navButton" type="button">{label}</button>
+  );
+}
+
+function NavigationBar() {
+  return(
+    <div id="navigationbar">
+      <br/><br/>
+      <Button label={"Om Vingulmark"}/>
+      <br/><br/>
+      <Button label={"Trening"}/>
+      <br/><br/>
+      <Button label={"Bilder og Media"}/>
+      <br/><br/>
+      <Button label={"Nybegynnertreninger"}/>
+      <br/><br/>
+      <Button label={"Kalender"}/>
+      <br/><br/>
+      <Button label={"Lenker"}/>
+      <br/><br/>
+      <Button label={"Kontaktinformasjon"}/>
+    </div>
+  );
+}
+
+function ImgHolder ({img, imgId, frame}) {
+  return(
+    <div id={frame}>
+      <img src={img} id={imgId} alt="test"></img>
+    </div>
+  );
+}
+
+function LinkIcon ({icon, link, idRef}) {
+  return(
+    <a href={link} id={idRef} target="_blank"><i class={icon}></i></a>
+  );
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Header headRef={headerIndex} headId="index-header" />
+        <NavigationBar />
+        <ImgHolder img={homeWelcome} imgId="welcomeImg" frame="welcomeFrame" />
       </div>
     );
   }
