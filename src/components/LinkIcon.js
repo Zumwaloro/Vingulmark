936df087e5d5f401e80afa07c4cc8f09c5e4f59a
Zumwaloro <bulattila@hotmail.com>
2018-12-16 19:15:24 +0100

    Added routing to the home button but it needs fixing yet

--- src/components/LinkIcon.js
+++ src/components/LinkIcon.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 function LinkIcon ({icon, link, idRef}) {
-    return(
-      <a href={link} target="_blank"><i class={icon} id={idRef}></i></a>
-    );
+    if (link !== undefined) {
+      return(
+        <a href={link} target="_blank"><i class={icon} id={idRef}></i></a>
+      );
+    } else {
+      return(
+        <Link to="/" target="_self">
+          <i class={icon} id={idRef}></i>
+        </Link>
+      );    
+    }
   }
 
   export default LinkIcon;
