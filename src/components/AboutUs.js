7901affe505e09df3eaf74d65f19154825779e30
Zumwaloro <bulattila@hotmail.com>
2018-12-16 17:25:23 +0100

    Added a home button to the header

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -11,12 +11,12 @@ import about1 from '../img/stock/about-1.png';
 import '../style/About.css';
 
 const input1 =
-    "Vingulmark ble stiftet i 2006 med formålet om å fremme kampsporten “hærkamp”, med fokus på vikingtid, i Oslo-området."+
+    "Vingulmark ble stiftet i 2006 med formålet om å fremme kampsporten “hærkamp”, med fokus på vikingtid, i Oslo-området. "+
     "Målet er at Vingulmark skal kunne tilby sine  medlemmer jevnlige treninger, felles historisk leirutstyr, reiser til vikingmarkeder"+
-    " i inn -og utland, samt et godt sosialt miljø. Vingulmark er også medlem av Nord-alliansen som er et samarbeid mellom de"+
+    " i inn-, og utland, samt et godt sosialt miljø. Vingulmark er også medlem av Nord-alliansen som er et samarbeid mellom de"+
     " største vikingkampgruppene i Norge. Nord har som mål å øke deltagelsen av nordmenn på treningstreff utenfor Norge, da"+
     " særlig på Moesgård i Danmark, som er det desidert største av sitt slag i Europa."+
-    "I 2009 ble Vingulmark slått sammen med den Oslo-baserte vikingkampgruppen Vandræðafélagit og har etter dette fått en ny"+
+    " I 2009 ble Vingulmark slått sammen med den Oslo-baserte vikingkampgruppen Vandræðafélagit og har etter dette fått en ny"+
     " økning av aktivitet på treninger, rekruttering av nye medlemmer, kampshow og andre prosjekter. Vingulmark er nå den største"+
     " vikingkampgruppen i Oslo og omegn med over tjue stridende medlemmer.";
 
@@ -27,6 +27,8 @@ class AboutUs extends Component {
           <Header headRef={headerAbout} headId="about-header" />
           <NavigationBar />
           <div id="textDiv">
+            <TextElement text={input1} id="text-first" />
+            <ImgHolder img={about1} imgId="about1" frame="frame1"/>
             <TextElement text={input1} id="text-first" />
             <ImgHolder img={about1} imgId="about1" frame="frame1"/>
           </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,7 @@ function Header({headRef, headId}) {
         <header id="home">
           <img src={headRef} id={headId}></img> 
           <LinkIcon icon="fab fa-facebook-square" link="https://www.facebook.com/Vingulmarkhaerkampgruppe/" idRef="face-icon" />
+          <LinkIcon icon="fas fa-home" link="" idRef="home-icon" />
         </header>
     
     );
