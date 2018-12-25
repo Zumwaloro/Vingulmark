e9ab9575fcef0611b5eb5390fe5188b1ed4c3d9c
Zumwaloro <bulattila@hotmail.com>
2018-12-25 19:49:05 +0100

    Corrected the positioning of elements on the About us page

--- src/components/pages/AboutUs.js
+++ src/components/pages/AboutUs.js
@@ -34,9 +34,7 @@ const input3 =
     " under vingene. Det første som møtte oss da vi ankom treningslokalene var en hærfugl som fortsatte og sirkle rundt"+
     " kampplassen i en drøy time før den fløy videre. Hærfuglen hekker opprinnelig i Sør-Europa og Midt-Østen og er svært sjeldent"+
     " sett her til lands. Ifølge “gammel norsk overtro” og andre regionale myter skal ankomsten av en hærfugl være et forvarsel på"+
-    " krig og død – derav det norske navnet.";
-
-    const input4 =
+    " krig og død – derav det norske navnet.\n\n"+
     "Med bakgrunn i hærfuglens tilstedeværelse på vårt jubileum og fuglens episke navn og mytologi, fant vi fort ut at dette var"+
     " maskoten vi var ute etter. Det er derfor hærfuglen som pryder vårt banner når Vingulmark drar ut i strid.";
 
@@ -47,14 +45,14 @@ class AboutUs extends Component {
           <Header headRef={headerAbout} headId="about-header" />
           <NavigationBar />
           <div id="textDiv">
-            <TextElement text={input1} id="text-first" />
             <ImgHolder img={about1} imgId="about1" frame="frame1"/>
+            <TextElement text={input1} id="text" />
             <ImgHolder img={about2} imgId="about2" frame="frame2"/>
-            <TextElement text={input2} id="text-second" />
-            <TextElement text={input3} id="text-third" />
-            <ImgHolder img={about3} imgId="about3" frame="frame3"/>
-            <ImgHolder img={about4} imgId="about4" frame="frame4"/>
-            <TextElement text={input4} id="text-fourth" />
+            <TextElement text={input2} id="text" />
+            <ImgHolder img={about3} imgId="about2" frame="frame2"/>
+            <TextElement text={input3} id="text" />
+            <ImgHolder img={about4} imgId="about2" frame="frame3"/>
+            <br/>
           </div>
         </div>
       );
