48d295fc3d9012161ef92d7cdbf65f257a5100c9
Zumwaloro <bulattila@hotmail.com>
2018-12-25 18:20:45 +0100

    Changed a button to Bli medlem and added new header; added content to the same page

--- src/components/pages/Join.js
+++ src/components/pages/Join.js
@@ -46,7 +46,7 @@ const input1 = "Kunne du tenke deg å prøve “hærkamp” eller kjenner du noe
               "5. BOLK:\n"+
               "Introduksjon av pålevåpen.\n"+
               "6. BOLK:\n"+
-              "Eventuell repetisjon\n\n"+
+              "Eventuell repetisjon.\n\n"+
 
               "Ta med:\n"+
               "-Hansker som gir beskyttelse mot slag (bandyhansker er topp)\n"+
@@ -60,6 +60,64 @@ const input1 = "Kunne du tenke deg å prøve “hærkamp” eller kjenner du noe
 
               "Vel møtt!";
 
+const input2 = "Bli medlem:\n" +
+
+"Ønsker du å trene hærkamp, reise på markeder og treningshelger og mekke historisk utstyr sammen med Vingulmark? Her kan du sjekke"+
+" kriterier vi har for rekruttering og hvor og hvordan man skal søke medlemskap.\n\n"+
+
+"Vingulmark rekrutterer for tiden på to forskjellige nivåer, tilpasset hvilket nivå av erfaring man ligger på og hvor mye man vil"+
+" investere i hobbyen. “Vingulmark” er hovedgruppen med relativt strenge krav, mens “Vingulmark-leidangen” er en undergruppe med"+
+" en forholdsvis lav terskel.\n\n"+
+
+"Vingulmark-leidangen:\n"+
+"Leidangen var historisk en organisering i vikingtiden for å kalle inn bondekrigere i ufredstid. Leidangen var ikke profesjonelle"+
+" krigere, men utgjorde likevel den største massen av krigere i fylkingen.\n"+
+
+"I Vingulmark er Leidangen en undergruppe med mål om å kunne inkludere de aller ferskeste krigerne og de som ikke ønsker å "+
+"investere så mye i reenactment-delen av foreningen. Medlemskap i Leidangen er tilstrekkelig for å kunne delta fullt ut på "+
+"hærkamp-delen av hobbyen.\n"+
+
+"Som medlem av Vingulmark-Leidangen har man lov til å male skjoldet sitt i Vingulmarks farger & mønster, kan reise og trene"+
+" sammen med Vingulmark på treningshelger og visse markeder, delta på Vingulmark-arrangementer som mekkekvelder og julebord, få"+
+" tilgang på Vingulmarks sosiale nettverk (facebook og mail) og har tale og forslagsrett (men ikke stemmerett) på Vingulmarks"+
+" møter.\n\n"+
+
+"Søknad og kriterier:\n"+
+"For å søke medlemsskap i Leidangen sender man en mail til styret i Vingulmark, vingulmark_styret[at]googlegroups[dott]com, der"+
+" man gjør rede for om man oppfyller kravene for medlemskap:\n"+
+"– At godkjent sikkerhetskurs e.l. har blitt gjennomført.\n"+
+"– At man eier eget skjold og våpen for hærkampstrid.\n"+
+"– At man er minst 16 år gammel (de under 18 år trenger underskrift fra foresatte).\n\n"+
+
+"Styret avgjør om hvorvidt søkeren kvalifiserer seg til medlemskap. For at medlemskap i Leidangen skal være gyldig må også"+
+" kontingenten [150kr] være betalt innen 2 måneder for inneværende år eller innen 2 måneder av opptak.";
+
+const input3 = "Vingulmark:\n"+
+"Den originale Vingulmark har tradisjonelt favnet mye av vikingtid-reenactmenten i tillegg til hærkamp. Vingulmark tar også"+
+" betalte formidlings/kampshow-oppdrag som setter høyere krav til utstyr, evner og bidragsvilje.\n\n"+
+
+"Vingulmark er hovedgruppen/den harde kjernen i foreningen som består av de som legger mest tid i hobbyen. Det er også Vingulmark"+
+" som står for organiseringen av foreningen i sin helhet. Det er forventet større deltagelse (show, dugnader, kjøringer, møter)"+
+" av medlemmene i Vingulmark enn Leidangen.\n\n"+
+
+"Som medlem av Vingulmark har man fulle rettigheter mht. vedtektene (nederst på siden), som i tillegg til rettighetene nevnt for"+
+" leidangen blant annet gir stemmerett på Vingulmarks møter.\n\n"+
+
+"Søknad og kriterier:\n"+
+"For å søke medlemsskap i Vingulmark sender man en mail til styret i Vingulmark, vingulmark_styret[at]googlegroups[dott]com, der\n"+
+" man gjør rede for om man oppfyller kravene for medlemskap:\n"+
+"– Godkjent sikkerhetskurs e.l.\n"+
+"– Minst 16 år (de under 18 trenger underskrift fra foresatte)\n"+
+"– Eie godkjent vikingdrakt, skjold og hærkampvåpen.\n\n"+
+
+"Søknad om medlemsskap avgjøres på medlems- eller årsmøte, der det trengs 2/3 flertall for godkjenning. I denne avgjørelsen"+
+" er disse veiledende kriteriene avgjørende:\n"+
+"– Bør ha reist på tur (treningshelg/marked) med Vingulmark og/eller deltatt på arrangement i Vingulmarks regi.\n"+
+"– Være aktiv på oslotreningene, dvs ca 2 ganger i måneden.\n"+
+"– Ha en interesse av vikingtid-reenactment.\n\n"+
+
+"For at medlemskap i Vingulmark skal være gyldig må også kontingenten [350kr] være betalt innen 2 måneder av det inneværende året.";
+
 class Join extends Component {
     render() {
       return (
@@ -68,7 +126,11 @@ class Join extends Component {
           <NavigationBar />
           <ImgHolder img={join1} imgId="joinImg" frame="imgFrame"/>
           <TextElement text={input1} id="text-input" />
-          <ImgHolder img={join1} imgId="joinImg" frame="imgFrame"/>
+          <ImgHolder img={join2} imgId="joinImg2" frame="imgFrame2"/>
+          <TextElement text={input2} id="text-input" />
+          <ImgHolder img={join3} imgId="joinImg2" frame="imgFrame2"/>
+          <TextElement text={input3} id="text-input" />
+          <ImgHolder img={join4} imgId="joinImg2" frame="imgFrame3"/>
           <br/>
         </div>
       );
